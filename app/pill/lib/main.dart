baedc5f6be51a06db5dd38d4fb212aa2dc4e1272
Seunghwanly <seunghwanly@gmail.com>
2021-01-14 11:37:54 +0900

    bottom navigation bar added

--- app/pill/lib/main.dart
+++ app/pill/lib/main.dart
@@ -12,13 +12,26 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  int pageIndex = 0;
+
+  changePage(int index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: '이건뭐약',
         home: Scaffold(
+          body: Center(
+            child: pages(pageIndex),
+          ),
           bottomNavigationBar: BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
+            currentIndex: pageIndex,
+            onTap: (int index) => changePage(index),
             items: [
               BottomNavigationBarItem(
                   label: "홈",
@@ -39,5 +52,18 @@ class _AppState extends State<App> {
             ],
           ),
         ));
+  }
+
+  pages(int index) {
+    switch(index) {
+      case 0:
+        return Text("HOME SCREEN");
+      case 1:
+        return Text("MAP SCREEN");
+      case 2:
+        return Text("CALENDAR SCREEN");
+      case 3:
+        return Text("PERSON SCREEN");
+    }
   }
 }
