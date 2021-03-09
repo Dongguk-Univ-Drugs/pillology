8e6aaa69a76032319de59c5df7e2023b130f8e00
Seunghwanly <seunghwanly@gmail.com>
2021-03-09 14:46:47 +0900

    [server] search REST API init

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const user = require('./users/index');
+// search
+const search = require('./search/index');
+router.use('/search', search);
 
+// user
+const user = require('./users/index');
 router.use('/user', user);
 
 module.exports = router;
